efccb38883268122b40aedfc2f73839d4b16a80b
Jorge Joaquin Garcia Pizarro <joaquingarcia757575@gmail.com>
2025-11-10 13:22:52 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// src/firebase/config.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "tu-api-key",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "tu-sender-id",
+  appId: "tu-app-id"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
